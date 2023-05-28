c0db288a99015b210fd103eef9d8c9c8970efdcb
AndrejSH1981 <133521547+AndrejSH1981@users.noreply.github.com>
2023-05-28 17:51:27 +0300

    Add files via upload

---
+++ Алгоритм/Алгоритм/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+public class Program
+{
+    
+    public static void Main()
+    {
+       
+       double x = Convert.ToDouble(Console.ReadLine());
+       Koren(x);
+        double flag = Koren(x);
+
+        Console.WriteLine(flag * flag);
+        
+    }
+    public static double Koren(double x)
+    {
+        double x1 = Math.Sqrt(x);
+        return(x1);
+    }
+}
